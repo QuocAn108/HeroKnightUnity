9ed2ce465588808b846098a43f090ba53098353e
AnHQ108 <anhq@hisoft.vn>
2025-05-30 08:19:48 +0700

    Initial commit

---
+++ Assets/Player.cs
@@ -0,0 +1,69 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Animator animator;
+    public Rigidbody2D rd;
+    public float jumpHeight = 5f;
+    
+    private float movement;
+    public float speed = 5f;
+    private bool facingRight = true;
+    private bool isGrounded = true;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement = Input.GetAxis("Horizontal");
+        if (movement < 0 && facingRight)
+        {
+            transform.eulerAngles = new Vector3(0, -180, 0);
+            facingRight = false;
+        }
+        else if (movement > 0 && !facingRight)
+        {
+            transform.eulerAngles = new Vector3(0, 0, 0);
+            facingRight = true;
+        }
+        if (Input.GetKey(KeyCode.Space) && isGrounded)
+        {
+            Jump();
+            isGrounded = false;
+            animator.SetBool("Jump", true);
+        }
+        if (Mathf.Abs(movement) > 0)
+        {
+            animator.SetFloat("Run", 1);
+        }
+        else if (Mathf.Abs(movement) == 0)
+        {
+            animator.SetFloat("Run", 0);
+        }
+        if(Input.GetKey(KeyCode.DownArrow))
+        {
+            animator.SetTrigger("Attack");
+        }
+        
+    }
+    private void FixedUpdate()
+    {
+        transform.position += new Vector3(movement, 0, 0) * speed * Time.fixedDeltaTime;
+    }
+    private void Jump()
+    {
+        rd.AddForce(new Vector2(0, jumpHeight), ForceMode2D.Impulse);
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+            animator.SetBool("Jump", false);
+        }
+    }
+}
